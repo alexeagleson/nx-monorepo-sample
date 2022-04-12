62d04c7cc08f5622342896c4fbac77e3d4396523
Alex Eagleson <alexeagleson@gmail.com>
2022-04-12 00:36:49 -0400

    build: add antd theme to core app

--- core-app/pages/_app.tsx
+++ core-app/pages/_app.tsx
@@ -1,13 +1,22 @@
 import type { AppProps } from "next/app";
+import "antd/dist/antd.variable.min.css";
+import { theme } from "tsdx-sample-component-library";
+import { ConfigProvider } from "antd";
 import Navigation from "../components/layouts/Navigation";
 import "./globals.css";
 
+ConfigProvider.config({
+  theme,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div style={{ display: "flex", flexDirection: "row" }}>
-      <Navigation />
-      <Component {...pageProps} />
-    </div>
+    <ConfigProvider>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Navigation />
+        <Component {...pageProps} />
+      </div>
+    </ConfigProvider>
   );
 }
 
